bd903a82dfba5ae4d8eaac1f24793d0fb6cd082e
EYE MOTION <eyemotiongamestudio@gmail.com>
2019-10-31 21:47:07 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css'
 import HooksFunction from './components/HooksFunction';
 
+function Todo({ todo, index}) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  )
+}
+
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  } 
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        className="input" 
+        value={value} 
+        placeholder="Add Todo ..."
+        onChange={e => setValue(e.target.value)} 
+        />  
+    </form>
+  )
+}
+
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about React',
+      isComplated: false
+    },
+
+    {
+      text: 'Meet friend for lunch',
+      isComplated: false
+    },
+
+    {
+      text: 'Build really cool todo app',
+      isComplated: false
+    },
+
+  ]);
+
+  const addTodo = text => {
+    const NewTodos = [...todos, { text }];
+    setTodos(NewTodos);
+  }
+
   return (
     <div className="App">
-      <HooksFunction />
+      <div className="todo-list">
+        {/*todos.map((todo, index) => {
+          <Todo key={index} index={index} todo={todo} />
+        })*/}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
